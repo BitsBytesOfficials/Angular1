7888ea6728087ca94151cba23cb6db8b5fce400b
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-04-13 12:36:09 +0530

    Routes Define

--- inventory-client/src/app/app-routing.module.ts
+++ inventory-client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import {ProdutDetailsComponent  } from "./produt-details/produt-details.component";
+import {UpdateProductComponent  } from "./update-product/update-product.component";
+const routes: Routes = [{ path: '', redirectTo: 'product', pathMatch: 'full' },
+{ path: 'products', component: ProductListComponent },
+{ path: 'add', component: CreateProductComponent },
+{ path: 'details/:id', component: ProdutDetailsComponent },
+{ path: 'update/:id', component: UpdateProductComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
